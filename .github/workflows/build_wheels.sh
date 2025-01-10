3382e0991077a40e20cf0c9811b1c83e33168373
Dayou Zhang <zhan6350@umn.edu>
2025-01-10 00:04:32 -0600

    limit archs

--- .github/workflows/build_wheels.sh
+++ .github/workflows/build_wheels.sh
@@ -4,4 +4,4 @@ set -e
 
 python -m pip install -U pip
 pip install cibuildwheel
-CIBW_BUILD="cp3{7,8,9,1?}-*" python -m cibuildwheel --output-dir wheelhouse
+CIBW_ARCHS="x86_64" CIBW_BUILD="cp3{7,8,9,1?}-*" python -m cibuildwheel --output-dir wheelhouse
