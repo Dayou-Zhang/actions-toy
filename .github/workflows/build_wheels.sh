08f1281bfe6957901100333eab5197716a4465c6
Dayou Zhang <zhan6350@umn.edu>
2025-01-13 01:28:19 -0600

    only support manylinux

--- .github/workflows/build_wheels.sh
+++ .github/workflows/build_wheels.sh
@@ -4,5 +4,4 @@ set -e
 
 python -m pip install -U pip
 pip install cibuildwheel
-CIBW_ARCHS="x86_64" CIBW_BUILD="cp3{9,1?}-*" python -m cibuildwheel --output-dir wheelhouse
-CIBW_ARCHS="x86_64" CIBW_BUILD="cp31{2,3}-*" python -m cibuildwheel --output-dir wheelhouse
+CIBW_ARCHS="x86_64" CIBW_BUILD="cp3{9,1?}-manylinux*" python -m cibuildwheel --output-dir wheelhouse
