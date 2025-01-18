2cf305983c2b1f5e69b44526c81929a8c7ea2c69
Dayou Zhang <zhan6350@umn.edu>
2025-01-18 01:29:57 -0600

    clean up publish

--- .github/workflows/build_wheels.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-python -m pip install -U pip
-pip install cibuildwheel
-# CIBW_ARCHS="x86_64" CIBW_BUILD="cp3{9,1?}-manylinux*" python -m cibuildwheel --output-dir wheelhouse
-CIBW_ARCHS="x86_64" CIBW_BUILD="cp313-manylinux*" python -m cibuildwheel --output-dir wheelhouse
